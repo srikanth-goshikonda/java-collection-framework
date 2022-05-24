07ca9195e44092c232141d89ab277be8166a4ccb
Srikanth Goshikonda <srikanthgoshikonda@gmail.com>
2022-05-24 17:01:48 +0530

    Arraylist sorting using concrete class and lambda expression

---
+++ collection-framework/src/main/java/com/digital/arraylist/ArrayListSort.java
@@ -0,0 +1,49 @@
+package com.digital.arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArrayListSort {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList(Arrays.asList(5, 8, 0, 44, 95, 1));
+
+		// Ascending Order sorting using implementation
+		list.sort(new AscendingOrder());
+		System.out.println(list);
+
+		// Descending Order sorting using implementation
+		list.sort(new DescendingOrder());
+		System.out.println(list);
+
+		// Ascending Order using Lambda Expression
+		list.sort((o1, o2) -> o1.compareTo(o2));
+		System.out.println(list);
+
+		// Descending Order using Lambda Expression
+		list.sort((o1, o2) -> o2.compareTo(o1));
+		System.out.println(list);
+
+
+	}
+}
+
+class AscendingOrder implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o1.compareTo(o2);
+	}
+
+}
+
+class DescendingOrder implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o2.compareTo(o1);
+	}
+
+}
